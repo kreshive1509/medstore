0043b6b6c83bd0898ed32e8a64088709bc3f7e34
kreshive1509 <kreshivesrivastava@gmail.com>
2025-07-10 20:28:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart(name, price) {
+  const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Item added to cart!");
+}
